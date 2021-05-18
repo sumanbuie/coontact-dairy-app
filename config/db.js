a0a919c46db93d12f2c30d4484f18e252be2db02
suman1buie <mandalsuman760@gmail.com>
2021-05-18 12:07:08 +0530

    setup the database

--- config/db.js
+++ config/db.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURL");
 
-const connectDB = () => {
-  mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("mongodb connected"))
-    .catch((err) => {
-      console.log("something went wrong ", err.message);
-      process.exit(1);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log("mongodb connected");
+  } catch (err) {
+    console.log("something went wrong ", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
